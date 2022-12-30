015cbd7f640b0ce152f6b34530d38c59de756321
David Castro <david.castroanaya@gmail.com>
2022-12-30 10:22:16 -0600

    Updating CV

--- src/components/DownloadBtn.jsx
+++ src/components/DownloadBtn.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Pdf from '../assets/David-Castro-Resume.pdf'
 
 const DownloadBtn = () => {
   return (
     <div>
-        <a href='./assets/David-Castro-Resume.pdf' download='CV'>
+        <a href={Pdf} download='CV' target="_blank" rel='noreferrer'>
             <button
               class="px-10 py-4 bg-blue-500 rounded-full drop-shadow-lg text-xl border text-white duration-300 hover:bg-amber-500"
               >
